d8041155a0a6d515019a2b6d2eac6cc0f65af308
redwrasse <redwrasse@users.noreply.github.com>
2020-12-10 13:53:58 -0700

    remove Block, Stack classes, refactor to building_blocks wavenet objs

--- wavenetlike/building_blocks.py
+++ wavenetlike/building_blocks.py
@@ -1,6 +1,7 @@
 # building_blocks.py
 
 import torch
+from torch import __init__
 from torch.nn import functional as F
 
 
@@ -118,6 +119,7 @@ class ResidualBlock(torch.nn.Module):
 
     def __init__(self, model):
         super(ResidualBlock, self).__init__()
+        self.model = model
 
     def forward(self, x):
         out = x + self.model(x)
@@ -180,4 +182,98 @@ class SkipPostProcessing(torch.nn.Module):
         return out_b
 
 
+class WavenetLike(torch.nn.Module):
 
+    def __init__(self,
+                 audio_channel_size,
+                 dilation_rate,
+                 filter_gate_kernel_size,
+                 filter_gate_out_channels,
+                 one_conv_residual_out_channels,
+                 one_conv_skip_out_channels,
+                 block_n_layers,
+                 n_blocks):
+        super(WavenetLike, self).__init__()
+
+        self.receptive_field_size = sum(dilation_rate**i for i in range(block_n_layers)) * n_blocks
+        self.in_out_channel_size = audio_channel_size
+
+        # add a pre 1-1 conv to change input discretization
+        # (from 256 down/up to intermediate channel dimension)
+        self.pre_one_conv = building_blocks.OneConv(
+            in_channels=audio_channel_size,
+            out_channels=one_conv_residual_out_channels
+        )
+
+        self.layers = torch.nn.ModuleList(
+            [WavenetLikeLayer(
+                in_channels=one_conv_residual_out_channels,
+                dilation=dilation_rate ** (
+                            i % block_n_layers),
+                filter_gate_out_channels=filter_gate_out_channels,
+                filter_gate_kernel_size=filter_gate_kernel_size,
+                one_conv_residual_out_channels=one_conv_residual_out_channels,
+                one_conv_skip_out_channels=one_conv_skip_out_channels
+            ) for i in range(block_n_layers *
+                             n_blocks)]
+        )
+        self.skip_pp = building_blocks.SkipPostProcessing(
+            in_channels=one_conv_skip_out_channels,
+            intermediate_channels=one_conv_skip_out_channels,
+            out_channels=audio_channel_size)
+
+    def forward(self, x):
+        pre_x = self.pre_one_conv(x)
+        skip_outs = []
+        dense_outs = [pre_x]
+        for i, layer in enumerate(self.layers):
+            out = layer(dense_outs[i])
+            dense_outs.append(out[0])
+            skip_outs.append(out[1])
+        skip_sum = torch.sum(torch.stack(skip_outs), dim=0)
+        out = self.skip_pp(skip_sum)
+        return out
+
+
+class WavenetLikeLayer(torch.nn.Module):
+
+    def __init__(self,
+                 in_channels,
+                 dilation,
+                 filter_gate_out_channels,
+                 filter_gate_kernel_size,
+                 one_conv_residual_out_channels,
+                 one_conv_skip_out_channels):
+        super(WavenetLikeLayer, self).__init__()
+        self.gau = building_blocks.GatedActivationUnit()
+        self.filter_conv = building_blocks.LpConv(
+            in_channels=in_channels,
+            out_channels=filter_gate_out_channels,
+            kernel_size=filter_gate_kernel_size,
+            dilation=dilation
+        )
+        self.gate_conv = building_blocks.LpConv(
+            in_channels=in_channels,
+            out_channels=filter_gate_out_channels,
+            kernel_size=filter_gate_kernel_size,
+            dilation=dilation
+        )
+        self.residual_one_conv = building_blocks.OneConv(
+            in_channels=filter_gate_out_channels,
+            out_channels=one_conv_residual_out_channels,
+            bias=True
+        )
+        self.skip_one_conv = building_blocks.OneConv(
+            in_channels=filter_gate_out_channels,
+            out_channels=one_conv_skip_out_channels,
+            bias=True
+        )
+
+    def forward(self, x):
+        wf = self.filter_conv(x)
+        wg = self.gate_conv(x)
+        z = self.gau(wf, wg)
+        residual_one_conv_out = self.residual_one_conv(z)
+        skip_out = self.skip_one_conv(z)
+        dense_out = residual_one_conv_out + x
+        return dense_out, skip_out
--- wavenetlike/models.py
+++ wavenetlike/models.py
@@ -4,7 +4,8 @@
 """
 import building_blocks
 import constants
-import torch
+
+from building_blocks import WavenetLike
 
 
 def build_wavenet_like(audio_channel_size,
@@ -16,84 +17,17 @@ def build_wavenet_like(audio_channel_size,
                        block_n_layers,
                        n_blocks):
 
-    class WavenetLike(torch.nn.Module):
+    return WavenetLike(
+        audio_channel_size=audio_channel_size,
+        filter_gate_kernel_size=filter_gate_kernel_size,
+        dilation_rate=dilation_rate,
+        filter_gate_out_channels=filter_gate_out_channels,
+        one_conv_residual_out_channels=one_conv_residual_out_channels,
+        one_conv_skip_out_channels=one_conv_skip_out_channels,
+        block_n_layers=block_n_layers,
+        n_blocks=n_blocks
 
-        def __init__(self):
-            super(WavenetLike, self).__init__()
-
-            self.receptive_field_size = sum(dilation_rate**i for i in range(block_n_layers)) * n_blocks
-            self.in_out_channel_size = audio_channel_size
-
-            # add a pre 1-1 conv to change input discretization
-            # (from 256 down/up to intermediate channel dimension)
-            self.pre_one_conv = building_blocks.OneConv(
-                in_channels=audio_channel_size,
-                out_channels=one_conv_residual_out_channels
-            )
-
-            self.layers = torch.nn.ModuleList(
-                [WavenetLikeLayer(
-                    in_channels=one_conv_residual_out_channels,
-                    dilation=dilation_rate ** (
-                                i % block_n_layers)
-                ) for i in range(block_n_layers *
-                                 n_blocks)]
-            )
-            self.skip_pp = building_blocks.SkipPostProcessing(
-                in_channels=one_conv_skip_out_channels,
-                intermediate_channels=one_conv_skip_out_channels,
-                out_channels=audio_channel_size)
-
-        def forward(self, x):
-            pre_x = self.pre_one_conv(x)
-            skip_outs = []
-            dense_outs = [pre_x]
-            for i, layer in enumerate(self.layers):
-                out = layer(dense_outs[i])
-                dense_outs.append(out[0])
-                skip_outs.append(out[1])
-            skip_sum = torch.sum(torch.stack(skip_outs), dim=0)
-            out = self.skip_pp(skip_sum)
-            return out
-
-    class WavenetLikeLayer(torch.nn.Module):
-
-        def __init__(self, in_channels, dilation):
-            super(WavenetLikeLayer, self).__init__()
-            self.gau = building_blocks.GatedActivationUnit()
-            self.filter_conv = building_blocks.LpConv(
-                in_channels=in_channels,
-                out_channels=filter_gate_out_channels,
-                kernel_size=filter_gate_kernel_size,
-                dilation=dilation
-            )
-            self.gate_conv = building_blocks.LpConv(
-                in_channels=in_channels,
-                out_channels=filter_gate_out_channels,
-                kernel_size=filter_gate_kernel_size,
-                dilation=dilation
-            )
-            self.residual_one_conv = building_blocks.OneConv(
-                in_channels=filter_gate_out_channels,
-                out_channels=one_conv_residual_out_channels,
-                bias=True
-            )
-            self.skip_one_conv = building_blocks.OneConv(
-                in_channels=filter_gate_out_channels,
-                out_channels=one_conv_skip_out_channels,
-                bias=True
-            )
-
-        def forward(self, x):
-            wf = self.filter_conv(x)
-            wg = self.gate_conv(x)
-            z = self.gau(wf, wg)
-            residual_one_conv_out = self.residual_one_conv(z)
-            skip_out = self.skip_one_conv(z)
-            dense_out = residual_one_conv_out + x
-            return dense_out, skip_out
-
-    return WavenetLike()
+    )
 
 
 def build_wavenet():
@@ -123,9 +57,13 @@ def build_wavenet_toy():
 
 
 def build_ar2():
-    # TODO("get rid of Stack abstraction")
-    stack = building_blocks.Block(n_layers=1, kernel_length=2, dilation_rate=1)
-    return stack.model
+    model = building_blocks.LpConv(
+        in_channels=1,
+        out_channels=1,
+        kernel_size=2,
+        dilation=1
+    )
+    return model
 
 
 def build_custom_model():
